a133d5bb45273433d1457634ea5b81fd0444ef3f
Paz-2007 <mariapazlucia2007@gmail.com>
2026-09-12 09:57:04 -0300

    clase planeta

---
+++ src/Planeta.ts
@@ -0,0 +1,9 @@
+export class Planeta {
+    nombre: string;
+    masa: number;
+
+    constructor(nombre: string, masa: number) {
+        this.nombre = nombre;
+        this.masa = masa;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+import class Planeta from './Planeta';
